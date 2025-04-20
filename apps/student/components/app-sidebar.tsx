b23af594810a72f6c9081ac681b62f957b7dd838
Alex <truonggiang190689@gmail.com>
2025-04-21 00:16:30 +0700

    feat: implement AppSidebar with collapsible navigation; add NavMain and NavProjects components; integrate address formatting and dropdown menu

--- apps/student/components/app-sidebar.tsx
+++ apps/student/components/app-sidebar.tsx
@@ -1,13 +1,46 @@
+"use client";
+
+import { SidebarData } from "@/constant";
+import { formatAddress } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@workspace/ui/components/dropdown-menu";
+import { Separator } from "@workspace/ui/components/separator";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarRail,
+  useSidebar,
 } from "@workspace/ui/components/sidebar";
-import { GalleryVerticalEnd } from "lucide-react";
+import { generateColorFromAddress } from "@workspace/ui/lib/utils";
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  CreditCard,
+  GalleryVerticalEnd,
+  LogOut,
+  Sparkles,
+} from "lucide-react";
+import { useAccount } from "wagmi";
+import { NavMain } from "./nav-main";
+import { NavProjects } from "./nav-projects";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const account = useAccount();
+  const { isMobile } = useSidebar();
+
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
@@ -26,8 +59,94 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </div>
         </div>
       </SidebarHeader>
-      <SidebarContent></SidebarContent>
-      <SidebarFooter></SidebarFooter>
+      <Separator />
+      <SidebarContent>
+        <NavMain items={SidebarData.navMain} />
+        <Separator />
+        <NavProjects projects={SidebarData.more} />
+      </SidebarContent>
+      <Separator />
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                  disabled={!account.address}
+                >
+                  <div
+                    className="bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-full dark:bg-neutral-800"
+                    style={{
+                      background: generateColorFromAddress(
+                        account.address ?? "",
+                      ),
+                    }}
+                  />
+                  <span className="truncate font-semibold">
+                    {account.address
+                      ? formatAddress(account.address)
+                      : "Your address..."}
+                  </span>
+                  <ChevronsUpDown className="ml-auto size-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                side={isMobile ? "bottom" : "right"}
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <div
+                      className="bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-full dark:bg-neutral-800"
+                      style={{
+                        background: generateColorFromAddress(
+                          account.address ?? "",
+                        ),
+                      }}
+                    />
+                    <span className="truncate font-semibold">
+                      {account.address
+                        ? formatAddress(account.address)
+                        : "Please login"}
+                    </span>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <Sparkles />
+                    Upgrade to Pro
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <BadgeCheck />
+                    Account
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCard />
+                    Billing
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Bell />
+                    Notifications
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <LogOut />
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
---
+++ apps/student/constant/sidebar.ts
@@ -0,0 +1,104 @@
+import {
+  BookText,
+  Bot,
+  FileText,
+  Map,
+  PenLine,
+  Settings,
+  UserCircle2,
+} from "lucide-react";
+
+export const SidebarData = {
+  navMain: [
+    {
+      title: "Discover",
+      url: "#",
+      icon: Map,
+      isActive: true,
+      items: [
+        // {
+        //   title: "History",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Starred",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Settings",
+        //   url: "#",
+        // },
+      ],
+    },
+
+    {
+      title: "Files",
+      url: "#",
+      icon: FileText,
+      items: [
+        // {
+        //   title: "Introduction",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Get Started",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Tutorials",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Changelog",
+        //   url: "#",
+        // },
+      ],
+    },
+    {
+      title: "Studio",
+      url: "#",
+      icon: PenLine,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "Billing",
+          url: "#",
+        },
+        {
+          title: "Limits",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Profile",
+      url: "#",
+      icon: UserCircle2,
+      items: [],
+    },
+  ],
+  more: [
+    {
+      name: "Resources",
+      url: "#",
+      icon: BookText,
+    },
+    {
+      name: "Models",
+      url: "#",
+      icon: Bot,
+    },
+    {
+      name: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ],
+};
