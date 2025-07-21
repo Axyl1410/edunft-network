71a9b7081cb162d11865616f51a9193070f4fa3f
Alex <truonggiang190689@gmail.com>
2025-07-21 23:09:45 +0700

    fix: update marketplace_address to new contract address for accurate deployment

--- apps/student/constant/contract.ts
+++ apps/student/constant/contract.ts
@@ -1,4 +1,3 @@
-export const marketplace_address = "0xa7d2c8d1Fd78cc65aaC5DBCfd91A4B7190acf89A";
+export const marketplace_address = "0xE6F633F43fFDB441619D180FE88a9008DF9921a9";
 
-export const questionRewardAddress =
-  process.env.NEXT_PUBLIC_QUESTION_REWARD_ADDRESS || "";
+export const questionRewardAddress = process.env.NEXT_PUBLIC_QUESTION_REWARD_ADDRESS || "";
